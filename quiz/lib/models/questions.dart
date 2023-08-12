f133b5093ba8f69bee13d420d529284b0927ca38
Roger-Pedroso <94937235+Roger-Pedroso@users.noreply.github.com>
2023-08-12 18:10:20 -0300

    quiz_app first commit

---
+++ quiz/lib/models/questions.dart
@@ -0,0 +1,21 @@
+import 'package:quiz/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+      correctAnswerIndex: 1,
+      options: ['a) Madrid', 'b) Paris', 'c) Berlin', 'd) Rome'],
+      question: '1. Qual é a capital da França?'),
+  Question(
+      correctAnswerIndex: 1,
+      options: [
+        'a) São Paulo',
+        'b) Brasilia',
+        'c) Rio de Janeiro',
+        'd) Curitiba'
+      ],
+      question: '2. Qual é a capital do Brasil?'),
+  Question(
+      correctAnswerIndex: 1,
+      options: ['a) São Paulo', 'b) Roma', 'c) Londres', 'd) Berlin'],
+      question: '3. O Rato roeu a roupa do rei de?'),
+];
